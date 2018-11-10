b2b4bb8f5433a80beca6706742b64f515f3f3048
用户名 <>
2018-11-10 19:34:42 +0800

    app.py大体框架

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-import logging; logging.basicConfig(level=logging.info)
+import asyncio, os, json, time
+from datetime import datetime
+import logging; logging.basicConfig(level=logging.INFO)
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(text='<h1>awesome blog</h1>',content_type='text/html')
+
+def second(request):
+    return web.Response(text='<h2>second</h2>',content_type='text/html')
+
+app = web.Application()
+app.add_routes([
+    web.get('/', index),
+    web.get('/se', second),
+])
+
+web.run_app(app=app,
+            host='127.0.0.1',
+            port=8000)
