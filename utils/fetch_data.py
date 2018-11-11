90fc28dfed303a322eed8259f507f8085edbe402
dbb2xbb <zs1102105@163.com>
2018-11-11 21:59:22 +0800

    添加utils/fetch_data.py beautifulSoup获取数据。
    Signed-off-by: dbb2xbb <zs1102105@163.com>

---
+++ utils/fetch_data.py
@@ -0,0 +1,53 @@
+import json
+import copy
+from bs4 import BeautifulSoup
+import re
+from urllib import request
+import traceback
+
+
+def get_html_content(url='http://www.baidu.com'):
+    try:
+        with request.urlopen(url) as f:
+            data = f.read()
+            # print('data:{}'.format(data.decode('utf-8')))
+            return data
+    except:
+        traceback.print_exc()
+
+def parse_html_content(content):
+    alist = []
+    soup = BeautifulSoup(content, "html.parser", from_encoding='utf-8')
+    tbody = soup.find_all('tr')
+    for idx,tr in enumerate(tbody):
+        if idx > 0:
+            anime_info = {
+                'year':0,
+                'name':[]
+            }
+            anime_info['year'] = tr.contents[0].text.replace('\n','')
+            for a in tr.find_all('a'):
+                anime_info['name'].append(a.string)
+
+            alist.append(anime_info)
+
+    return json.dumps(alist)
+
+def show_res(alist):
+    print(alist)
+    # for item in alist:
+    #     for k,v in item:
+    #         print(k, v)
+
+def main():
+    url = 'https://baike.baidu.com/item/%E5%90%8E%E5%AE%AB%E5%8A%A8%E6%BC%AB/8550893?fr=aladdin'
+    cnt = get_html_content(url=url)
+    res = parse_html_content(cnt)
+    show_res(res)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
